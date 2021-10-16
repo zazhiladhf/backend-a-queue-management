b079066fee8f5e763259a9a5e4f2e63b3c4c24b0
Rizqi Pratama <rizqi.mailsos@gmail.com>
2021-10-16 15:57:07 +0700

    add db rakamin

--- database/connection.go
+++ database/connection.go
@@ -15,10 +15,14 @@ func Connect() *gorm.DB {
 	// dsn := os.Getenv("CONNSTRING")
 	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// For Online DB Development
-	dsn := "sql6441433:K7cwfKcqVs@tcp(sql6.freesqldatabase.com:3306)/sql6441433"
+	// For Deployment Kominfo
+	dsn := "backend-a-queue-management:dKq8hLYhbWF8sMXd@tcp(35.201.240.187:3306)/backend-a-queue-management"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	
+
+	// For Online DB Development
+	// dsn := "sql6441433:K7cwfKcqVs@tcp(sql6.freesqldatabase.com:3306)/sql6441433"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
 	// For LocalDB Developmen
 	// dsn := "user:password@tcp(localhost:3306)/database"
 	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
